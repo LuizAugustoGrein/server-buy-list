7cd8e31d1a3a4926274902fcdd783616debe5663
luizaugustogrein <luizaugustogrein@gmail.com>
2023-05-15 20:17:13 -0300

    dotenv

--- src/lib/knex.ts
+++ src/lib/knex.ts
@@ -1,8 +1,8 @@
 export const knex = require('knex')({
   client: 'mysql2',
   connection: {
-    host : '127.0.0.1',
-    port : 30306,
+    host : '172.30.20.10',
+    port : 3306,
     user : 'root',
     password : 'root',
     database : 'buy_list'
